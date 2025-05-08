dfa5f9f1d983b9c6ab022ee822461202c049ceae
XuKexin <xkx867306789@qq.com>
2025-05-08 23:13:30 +0800

    Parse the request message

--- server/server.py
+++ server/server.py
@@ -16,3 +16,7 @@ def handle_client(client_socket, client_address):
             request = client_socket.recv(1024).decode('utf-8')
             if not request:
                 break
+            message_size = int(request[:3])
+            command = request[3]
+            key = request[4:message_size - 1] if command != 'P' else request[4:message_size - len(request.split(' ')[-1]) - 2]
+            value = request.split(' ')[-1] if command == 'P' else ''
