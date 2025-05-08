9416a86b6ebf5c4edbd3b83283b94235cfc8ec8b
XuKexin <xkx867306789@qq.com>
2025-05-08 22:59:43 +0800

    A function for handling client requests

--- server/server.py
+++ server/server.py
@@ -8,3 +8,8 @@ read_count = 0
 get_count = 0
 put_count = 0
 error_count = 0
+def handle_client(client_socket, client_address):
+    global client_count, operation_count, read_count, get_count, put_count, error_count
+    client_count += 1
+    while True:
+        try:
